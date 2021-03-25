79b3a230cfadd525ad27fc90fe70f413fefd26ca
Davi Batista <DaviBatista.gb@gmail.com>
2021-03-25 15:51:08 -0300

    [Davi] - alteracao no formGroup e disabled

--- src/app/modules/login/components/login-circle/login-circle.component.ts
+++ src/app/modules/login/components/login-circle/login-circle.component.ts
@@ -1,30 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import{ Login } from '../../../../shared/models/login'
-import { LoginService } from '../../../../shared/service/login.service'
+import { LoginService } from '../../../../shared/service/login.service';
 
 @Component({
   selector: 'app-login-circle',
   templateUrl: './login-circle.component.html',
-  styleUrls: ['./login-circle.component.css']
+  styleUrls: ['./login-circle.component.css'],
 })
 export class LoginCircleComponent implements OnInit {
-
-  form = new FormGroup({
-    funcional: new FormControl('', Validators.required),
-    racf: new FormControl('', Validators.required),
-    senha: new FormControl('', Validators.required)
-  })
+  public form: FormGroup;
 
   constructor(
-    private loginService: LoginService,
-    private router: Router
-  ) { 
-    
-  }
-  
+    private loginService: LoginService, //
+    private router: Router,
+    private fb: FormBuilder
+  ) {}
+
   ngOnInit(): void {
+    this.form = this.fb.group({
+      funcional: ['', [Validators.required]],
+      racf: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
   }
 
   onConsultar(): void {
--- src/app/ui/input/input.component.ts
+++ src/app/ui/input/input.component.ts
@@ -1,23 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
-
-
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.css'],
 })
 export class InputComponent implements OnInit {
-
-  @Input() control = new FormControl();
-
+  @Input() control = new FormControl('');
   @Input() campo: string;
   @Input() tipo: string;
- 
-  constructor() { }
-  
-  ngOnInit(): void {
-  }
 
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log(this.control);
+  }
 }
